b6dbf83e47210244f8231019d082e608f39142e5
Mogeeswaran <mogeeswaran1993@gmai.com>
2020-09-24 23:33:37 +0530

    Initial commit

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -36,6 +36,14 @@ public class Book {
         this.publisher = publisher;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
